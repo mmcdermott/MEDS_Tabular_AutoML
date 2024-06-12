c5b4b20ab7c9bc2f490b97074a8685b46d156140
Aleksia Kolo <aleksiakolo@gmail.com>
2024-06-12 15:55:28 -0400

    readability spacing

--- src/MEDS_tabular_automl/generate_summarized_reps.py
+++ src/MEDS_tabular_automl/generate_summarized_reps.py
@@ -193,6 +193,7 @@ def generate_summary(
             f"Invalid aggregation: {agg}. Valid options are: {CODE_AGGREGATIONS + VALUE_AGGREGATIONS}"
         )
     assert len(feature_columns), "feature_columns must be a non-empty list"
+
     ts_columns = get_feature_names(agg, feature_columns)
     # Generate summaries for each window size and aggregation
     code_type, _ = agg.split("/")
@@ -201,6 +202,7 @@ def generate_summary(
     logger.info(
         f"Generating aggregation {agg} for window_size {window_size}, with {len(ts_columns)} columns."
     )
+
     out_matrix = compute_agg(index_df, matrix, window_size, agg, len(ts_columns), use_tqdm=use_tqdm)
     return out_matrix
 
