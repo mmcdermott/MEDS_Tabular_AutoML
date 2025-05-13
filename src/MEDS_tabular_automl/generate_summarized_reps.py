b3400f63f68dc1cb34d520a3ae1fc01047673291
Matthew McDermott <mattmcdermott8@gmail.com>
2025-05-13 14:03:32 -0400

    Fixed pre-commit error

--- src/MEDS_tabular_automl/generate_summarized_reps.py
+++ src/MEDS_tabular_automl/generate_summarized_reps.py
@@ -153,10 +153,9 @@ def get_rolling_window_indicies(
 
         if "time" not in label_df.schema:
             label_df = label_df.rename({"prediction_time": "time"})
-            
+
         windows = (
-            label_df
-            .join_asof(event_df, by="subject_id", on="time")
+            label_df.join_asof(event_df, by="subject_id", on="time")
             .select(windows.columns)
             .fill_null(0)
             .collect()
@@ -283,7 +282,7 @@ def compute_agg(
     )
     index_df = group_df.lazy().select(pl.col("subject_id", "time"))
     windows = group_df.select(pl.col("min_index", "max_index"))
-    
+
     logger.info("Step 1.5: Running sparse aggregation.")
     matrix = aggregate_matrix(windows, matrix, agg, num_features, use_tqdm)
 
@@ -334,7 +333,7 @@ def generate_summary(
 
     ts_columns = get_feature_names(agg, feature_columns)
     # Generate summaries for each window size and aggregation
-    code_type, _ = agg.split("/") # code or value like in code/count
+    code_type, _ = agg.split("/")  # code or value like in code/count
     # only iterate through code_types that exist in the dataframe columns
     if not any(c.endswith(code_type) for c in ts_columns):
         raise ValueError(f"No columns found for aggregation {agg} in feature_columns: {ts_columns}.")
